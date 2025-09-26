d403c1d365849487848b3ea65ac62717495f9152
Julien Delsescaux <jdlxt@debian12>
2025-09-26 22:27:42 +0200

    kanban board

--- src/laravel/resources/js/kanban/KanbanState.js
+++
@@ -1,4 +0,0 @@
-// File moved to ./models/KanbanState.js
-// This file is intentionally left to avoid broken imports during transition.
-// Please update imports to 'models/KanbanState' and remove this stub if unused.
-export { default } from './models/KanbanState';
